dd4bd3b8434784bd4d67cbe0dd6473e7dbc0f647
Charles McCarthy <charles@studybundles.com>
2021-04-19 18:21:04 +0100

    Env SNS Topic Name

--- src/sns-sqs/publish.ts
+++ src/sns-sqs/publish.ts
@@ -1,9 +1,10 @@
 import * as AWS from 'aws-sdk';
+import { AWS_ACCESS_KEY_ID, AWS_ACCOUNT_ID, AWS_PRICE_SUGGESTIONS_SNS_TOPIC, AWS_REGION, AWS_SECRET_ACCESS_KEY_ID } from '../environment';
 
 AWS.config.update({
-	region: 'eu-west-1',
-	// accessKeyId: '...',
-	// secretAccessKey: '...'
+	region: AWS_REGION,
+	accessKeyId: AWS_ACCESS_KEY_ID,
+	secretAccessKey: AWS_SECRET_ACCESS_KEY_ID
 });
 
 export class SNSPublish {
@@ -11,21 +12,16 @@ export class SNSPublish {
 	public static send = (message: string): void => {
 		if (!message) throw Error('SNS message not sent - Message was empty');
 
-		const sns = new AWS.SNS({ apiVersion: '2010-03-31' });
+		const sns: AWS.SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
 
-		const params = {
+		const params: AWS.SNS.PublishInput = {
 			Message: message,
-			TopicArn: 'arn:aws:sns:eu-west-1:068475715603:TestTopic'
+			TopicArn: `arn:aws:sns:${AWS_REGION}:${AWS_ACCOUNT_ID}:${AWS_PRICE_SUGGESTIONS_SNS_TOPIC}`
 		};
 
-		sns.publish(params, (err, data) => {
-			if (err) {
-				console.error('Message Failed to Send')
-				console.error(err, err.stack);
-			} else {
-				console.log('Message Sent')
-				console.log(data);
-			}
+		sns.publish(params, (err: AWS.AWSError, data: AWS.SNS.PublishResponse): void => {
+			if (err) console.error('SNS Message Failed to Send', err, err.stack);
+			else console.log('SNS Message Sent', data);
 		});
 	}
 
