443168636176219cda43cbf88fb8cc9a03c32684
Charles McCarthy <charles@studybundles.com>
2021-04-20 08:24:41 +0100

    Get Price Alerts Endpoint

--- src/controllers/price-controller.ts
+++ src/controllers/price-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { MarketPriceListener } from '../listeners/market-price-listener';
 import { PriceEvaluator } from '../services/price-evaluator';
+import { PriceAlert, PriceAlerts } from '../services/price-alerts';
 
 export class PriceController {
 
@@ -16,6 +17,12 @@ export class PriceController {
 		const symbols: any = MarketPriceListener.StopListening();
 
 		return res.status(200).json({ success: true, symbols });
+	}
+
+	public static priceAlerts = async (req: Request, res: Response): Promise<Response> => {
+		const priceAlerts: PriceAlert[] = PriceAlerts.priceAlerts;
+
+		return res.status(200).json({ success: true, priceAlerts });
 	}
 
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,5 +6,6 @@ const indexRouter: Router = express.Router();
 indexRouter.get('/health', HealthController.health);
 indexRouter.get('/start', PriceController.start);
 indexRouter.get('/stop', PriceController.stop);
+indexRouter.get('/price-alerts', PriceController.priceAlerts);
 
 export default indexRouter;
