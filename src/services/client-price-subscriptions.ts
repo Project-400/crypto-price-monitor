250fe014bebf1a364254050ee820da30d6fe7635
Charles McCarthy <charles@studybundles.com>
2021-04-20 03:43:31 +0100

    Client Price Subscriptions Services

---
+++ src/services/client-price-subscriptions.ts
@@ -0,0 +1,22 @@
+export class ClientPriceSubscriptions {
+
+	private static subscribers: string[] = [];
+
+	public static AddSub = (socketId: string): void => {
+		console.log('Adding Price Subscriber');
+		ClientPriceSubscriptions.subscribers.push(socketId);
+	}
+
+	public static RemoveSub = (socketId: string): void => {
+		console.log('Removing Price Subscriber');
+		console.log(ClientPriceSubscriptions.subscribers);
+
+		const index: number = ClientPriceSubscriptions.subscribers.findIndex((s: string): boolean => s === socketId);
+		if (index >= 0) {
+			ClientPriceSubscriptions.subscribers.splice(index, 1);
+		}
+
+		console.log(ClientPriceSubscriptions.subscribers);
+	}
+
+}
